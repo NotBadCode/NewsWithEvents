66aba4673f56bfa0c2c6c9f1904e70605e8d2c61
Sergey <filinsrg@gmail.com>
2017-08-10 11:00:35 +0300

    news widget view

--- widgets/views/last-news.php
+++ widgets/views/last-news.php
@@ -10,12 +10,15 @@ use yii\helpers\Html;
 <h2>
     <?= Yii::t('app', 'Last News:') ?>
 </h2>
-<div class="news">
+<div class="news row">
     <?php foreach ($news as $newsItem): ?>
-        <div class="news-item">
+        <div class="news-item col-xs-12 col-sm-6 col-md-4">
             <h3>
                 <?= Html::encode($newsItem->title) ?>
             </h3>
+            <span class="date">
+                <?= Yii::$app->formatter->asDate($newsItem->create_time, 'php:Y-m-d'); ?>
+            </span>
             <div>
                 <?= Html::img($newsItem->getThumbUploadUrl('image', 'news_thumb'), ['class' => 'news-image']); ?>
                 <?= Html::encode($newsItem->short_text) ?>
